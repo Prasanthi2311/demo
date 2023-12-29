e98cee1a910a3e0c69b116b58f38de3bda6bf438
Prasanthi2311 <124231233+Prasanthi2311@users.noreply.github.com>
2023-12-29 11:57:11 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -5,6 +5,6 @@ st.image("./cat1.jpg")
 st.write("- Persian Cat")
 st.image("./cat2.jpg")
 st.write("- White Cat")
-st.image("https://yt3.ggpht.com/a/AGF-l7-3jWa9P55VQ8-0kYWPJLAyC6ZyUcpyHo-gNw%3ds900-c-k-c0xffffffff-no-rj-mo",width=250,use_column_width=250)
+st.image("https://yt3.ggpht.com/a/AGF-l7-3jWa9P55VQ8-0kYWPJLAyC6ZyUcpyHo-gNw%3ds900-c-k-c0xffffffff-no-rj-mo",width=300,use_column_width=250)
 st.write("- Burmese Cat")
 
