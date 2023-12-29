d35e282f4fc13e866ce5fdae42a921b12b13e58a
Prasanthi2311 <124231233+Prasanthi2311@users.noreply.github.com>
2023-12-29 12:09:17 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -3,12 +3,12 @@ st.set_page_config(page_title='cats')
 st.markdown("## Types of Cats")
 col1,col2,col3=st.columns(3)
 with col1:
-  st.image("./cat1.jpg",width=320)
+  st.image("./cat1.jpg",width=320,use_column_width=True)
   st.write("- Persian Cat")
 with col2:  
   st.image("./cat2.jpg")
-  st.write("- White Cat")
+  st.write("- White Cat",use_column_width=True)
 with col3:  
-  st.image("https://yt3.ggpht.com/a/AGF-l7-3jWa9P55VQ8-0kYWPJLAyC6ZyUcpyHo-gNw%3ds900-c-k-c0xffffffff-no-rj-mo",width=320)
+  st.image("https://yt3.ggpht.com/a/AGF-l7-3jWa9P55VQ8-0kYWPJLAyC6ZyUcpyHo-gNw%3ds900-c-k-c0xffffffff-no-rj-mo",width=320,use_column_width=True)
   st.write("- Burmese Cat")
 
