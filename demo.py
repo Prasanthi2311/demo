f2e147d035737e47111f0abd1e6e9e1a28bc2962
Prasanthi2311 <124231233+Prasanthi2311@users.noreply.github.com>
2023-12-29 11:37:05 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,7 +1,7 @@
 import streamlit as st
 st.set_page_config(page_title='cats')
 st.markdown("## Types of Cats")
+st.image("./Persian)
 st.write("- Persian Cat")
-st.image(image="https://th.bing.com/th/id/OIP.DR_wk-UnypY5RlJ73q9ImwHaE8?rs=1&pid=ImgDetMain", caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
+st.image("./Whitecat)
 st.write("- White Cat")
-st.image(image="https://www.bing.com/ck/a?!&&p=96b89b5dad2f629cJmltdHM9MTcwMzcyMTYwMCZpZ3VpZD0yOWRhYmE1OC0wYTg5LTY0ZDctMTMxMS1hYmM5MGI3ZTY1NTQmaW5zaWQ9NTg4OQ&ptn=3&ver=2&hsh=3&fclid=29daba58-0a89-64d7-1311-abc90b7e6554&u=a1L2ltYWdlcy9zZWFyY2g_cT1wZXJzaWFuIGNhdCZGT1JNPUlRRlJCQSZpZD1FQTAyRUREM0VFQjA1RkU0QTQ1RThFMjgzQ0U0MDAxNjBFQTJDOENE&ntb=1", caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
