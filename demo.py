ae126340fb1383e10937cfca2e0f96e6c7bf766b
Prasanthi2311 <124231233+Prasanthi2311@users.noreply.github.com>
2023-12-29 12:14:16 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -9,7 +9,7 @@ with col1:
 with col2:  
   st.image("./cat2.jpg")
   st.write("- White Cat",use_column_width=True)
-   st.write("White Cats Are Cute")
+  st.write("White Cats Are Cute")
 with col3:  
   st.image("https://yt3.ggpht.com/a/AGF-l7-3jWa9P55VQ8-0kYWPJLAyC6ZyUcpyHo-gNw%3ds900-c-k-c0xffffffff-no-rj-mo",width=320,use_column_width=True)
   st.write("- Burmese Cat")
